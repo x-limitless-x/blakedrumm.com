da163e79672400f51a51c32a97d0acc421b08426
Blake Drumm <bdrummtelco@gmail.com>
2022-05-02 00:04:21 -0400

    Update on-scroll-progress.js

--- assets/js/on-scroll-progress.js
+++ assets/js/on-scroll-progress.js
@@ -1,10 +1,8 @@
 document.addEventListener('scroll', _ => {
-  var docElem = document.documentElement;
-  var docBody = document.body;
-  var scrollTop = (docBody.scrollTop || docElem.scrollTop);
+  var scrollTop = (TopNav);
   var height = docElem.scrollHeight - docElem.clientHeight;
 
-  progress = scrollTop / height * 100;
+  progress = TopNav / height * 100;
 
   if ( progress > 0) {
     progressBar = document.querySelector('#progress-bar');
