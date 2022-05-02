8947faed939a93bc02525b567983dcedb2730129
Adam Drumm <Adrummtelco@gmail.com>
2022-05-02 13:44:42 -0400

    Update on-scroll-progress.js

--- assets/js/on-scroll-progress.js
+++ assets/js/on-scroll-progress.js
@@ -5,12 +5,17 @@ document.addEventListener('scroll', _ => {
   var height = docElem.scrollHeight - docElem.clientHeight;
 
   progress = scrollTop / height * 100;
+  
+  var progressBarExist = document.querySelector('#progress-bar') !== null;
 
   // only run if the element exists to be updated
-  if ( progress > 0 && document.querySelector('#progress-bar') !== null ) {
-    progressBar = document.querySelector('#progress-bar');
-    progressBar.style.setProperty('--progress', progress + '%');
-  } else {
-    progressBar.style.setProperty('--progress', '0%');
+  if ( progressBarExist )
+  {
+    if ( progress > 0 ) {
+      progressBar = document.querySelector('#progress-bar');
+      progressBar.style.setProperty('--progress', progress + '%');
+    } else {
+      progressBar.style.setProperty('--progress', '0%');
+    }
   }
 });
