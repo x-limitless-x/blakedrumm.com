976051f86271e82525586a0500f7422c782358f6
Blake Drumm <bdrummtelco@gmail.com>
2022-05-02 00:07:27 -0400

    Revert Change

--- assets/js/on-scroll-progress.js
+++ assets/js/on-scroll-progress.js
@@ -1,14 +1,10 @@
 document.addEventListener('scroll', _ => {
-  var scrollTop = (TopNav);
+  var docElem = document.documentElement;
+  var docBody = document.body;
+  var scrollTop = (docBody.scrollTop || docElem.scrollTop);
   var height = docElem.scrollHeight - docElem.clientHeight;
 
-  progress = TopNav / height * 100;
+  progress = scrollTop / height * 100;
 
   if ( progress > 0) {
     progressBar = document.querySelector('#progress-bar');
-    progressBar.style.setProperty('--progress', progress + '%');
-  } else {
-    progressBar.style.setProperty('--progress', '0%');
-  }
-});
-
